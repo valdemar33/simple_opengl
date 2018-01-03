4ac613c0a888d81ef9756586e63c66245fc4f397
Vladimir <valdemar-33@yandex.ru>
2018-01-03 16:53:31 +0300

    03/01

--- opengl3d.py
+++ opengl3d.py
@@ -40,11 +40,14 @@ def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display,DOUBLEBUF|OPENGL)
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0,0.0, -5)
+    gluPerspective(90, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0,0.0, -50)
     glRotatef(0, 0, 0, 0)
+    i = 0
 
     while True:
+        glTranslatef(0,0,i)
+        i += 0.00005
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -55,14 +58,18 @@ def main():
                     glTranslatef(-1,0,0)
                 if event.key == pygame.K_RIGHT:
                     glTranslatef(1,0,0)
+                if event.key == pygame.K_UP:
+                    glTranslatef(0,1,0)
+                if event.key == pygame.K_DOWN:
+                    glTranslatef(0,-1,0)
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4:
-                    glTranslatef(0,0,1.0)
-                if event.button == 5:
-                    glTranslatef(0,0,1.0)
+##            if event.type == pygame.MOUSEBUTTONDOWN:
+##                if event.button == 4:
+##                    glTranslatef(0,0,1.0)
+##                if event.button == 5:
+##                    glTranslatef(0,0,-1.0)
 
-        glRotatef(1, 3, 2, 1)
+        glRotatef(0, 0, 0, 0)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
